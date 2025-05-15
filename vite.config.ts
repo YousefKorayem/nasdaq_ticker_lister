a0230e3a0fe9ef9ee592e7cb30985768e77ff7d4
Yousef Korayem <yousefkorayem1@gmail.com>
2025-05-15 03:51:27 +0300

    adjusting for local server deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/YousefKorayem/nasdaq_ticker_lister',
+  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
   plugins: [
     react(),
   ],
