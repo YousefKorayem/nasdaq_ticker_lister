59202c584d291bc3eb476152f5324ca02b2c63ab
Yousef Korayem <yousefkorayem1@gmail.com>
2025-05-15 12:11:01 +0300

    adjusting routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/nasdaq-ticker-lister/' : '/',
   plugins: [
     react(),
   ],
